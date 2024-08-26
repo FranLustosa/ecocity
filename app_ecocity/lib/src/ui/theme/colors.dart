ce42ce25c290e357f7a36c6f079d6d16a12cdf23
FranLustosa <franciellelimalustosa@gmail.com>
2024-08-26 14:53:58 -0300

    commit

--- app_ecocity/lib/src/ui/theme/colors.dart
+++ app_ecocity/lib/src/ui/theme/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorSystem {
   // Cores
-  static const Color primaryColor = Color(0xFF00265F);
+  static const Color primaryColor = Color.fromARGB(255, 107, 175, 19);
   static const Color borderColor = Color(0xFF747474);
   static const Color textDestaqueColor = Color(0xFF505050);
   static const Color textColor = Color(0xFF000000);
--- app_ecocity/lib/src/view/PageLogin.dart
+++ app_ecocity/lib/src/view/PageLogin.dart
@@ -3,6 +3,7 @@
 // ignore_for_file: non_constant_identifier_names, prefer_const_constructors, deprecated_member_use
 
 import 'package:app_ecocity/main.dart';
+import 'package:app_ecocity/src/ui/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 class PageLogin extends StatefulWidget {
@@ -35,134 +36,35 @@ class _MyStatefulWidgetState extends State<PageLogin> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "BARQ",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 60,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                ),
+                "Seu app de sustentabilidade urbana",
               ),
-              Text(
-                "\n Seu cardápio Digital\n",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(height: 100),
+              SizedBox(height: 250),
               ElevatedButton(
                 // em caso de não aceitar os termos de uso, retorna o código abaixo
                 onPressed: () {
                   if (VerificarTermos()) {
                     Navigator.of(context)
                         .push(MaterialPageRoute(builder: (context) => MyApp()));
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text("Termos de uso não aceito!"),
-                          content: Text(
-                              "Querido(a) cliente, você deve aceitar os termos de uso para realizar seu pedido em nosso cardápio."),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text(
-                                "Fechar",
-                                style: TextStyle(
-                                  color: Color(0xFF00265F),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
-                    );
                   }
                 },
                 style: ElevatedButton.styleFrom(
-                  fixedSize: Size(370, 62),
-                  backgroundColor: Color(0xFF00265F),
+                  fixedSize: Size(300, 62),
+                  backgroundColor: ColorSystem.primaryColor,
                   primary: Colors.white,
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15),
                   ),
                 ),
                 child: Text(
-                  "Acessar Cardápio",
-                  style: TextStyle(fontSize: 20),
+                  "Acessar",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
                 ),
               ),
               SizedBox(height: 35),
-              ElevatedButton(
-                onPressed: () {
-                  if (VerificarTermos()) {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) => MyApp()));
-                  } else {
-                    // Popup de solicitar garçom
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text("Garçom solicitado!"),
-                            content:
-                                Text("Em instantes lhe atenderá, aguarde!"),
-                            actions: <Widget>[
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text(
-                                    "Fechar",
-                                    style: TextStyle(
-                                      color: Color(0xFF00265F),
-                                    ),
-                                  )),
-                            ],
-                          );
-                        });
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  fixedSize: Size(370, 62),
-                  backgroundColor: Colors.white,
-                  primary: Color(0xFF00265F),
-                  onPrimary: Color(0xFF00265F),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-                child: Text(
-                  "Solicitar Garçom",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
               SizedBox(height: 30),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Checkbox(
-                    value: isChecked,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        isChecked = value!;
-                      });
-                    },
-                    activeColor: Color(0xFF00265F),
-                    side: BorderSide(width: 1.5, color: Colors.white),
-                  ),
-                  Text(
-                    "Li e aceito os termos de uso",
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  ),
-                ],
-              )
             ],
           ),
         )
