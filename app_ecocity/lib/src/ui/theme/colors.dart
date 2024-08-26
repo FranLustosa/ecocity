87591b5d08e60aa86c8a4095d3faf74ea93ad40a
FranLustosa <franciellelimalustosa@gmail.com>
2024-08-26 13:01:55 -0300

    commit

---
+++ app_ecocity/lib/src/ui/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorSystem {
+  // Cores
+  static const Color primaryColor = Color(0xFF00265F);
+  static const Color borderColor = Color(0xFF747474);
+  static const Color textDestaqueColor = Color(0xFF505050);
+  static const Color textColor = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
